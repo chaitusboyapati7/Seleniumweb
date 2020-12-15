eaaaa230e0957bc606c49238a09beac8ed3e0627
chaitusboyapati7 <75864873+chaitusboyapati7@users.noreply.github.com>
2020-12-14 18:39:32 -0600

    Update Login.java
    Updated code changes

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -4,6 +4,8 @@ public class Login {
 	public static void  main(String[]args) {
 	System.out.println("Hello World");
 	System.out.println("FirstProgram");
+	System.out.println("Second test");
+	System.out.println("ThirdTest");
 
 	}
 }
